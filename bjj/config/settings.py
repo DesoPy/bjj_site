e03aa33a32a71db1ab7bb1cea35888104c24cc8f
Serhii <s.glotov1988@gmail.com>
2023-05-21 20:31:11 +0300

    0.0.1.3 Pre relise (change DB)

--- bjj/config/settings.py
+++ bjj/config/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'user': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bjj',
         'USER': 'Serhii',
         'PASSWORD': 'rootroot',
         'HOST': 'localhost',
         'PORT': 5432,
+    },
+    'user': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
